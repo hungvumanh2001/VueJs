113a017c9da2c5f960dda1745b2d5a5dd6a8e010
hungvm <hungvm@seatechit.com.vn>
2023-06-02 23:13:46 +0700

    fix lỗi firebase

--- i-money/src/configs/firebase.js
+++ i-money/src/configs/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
